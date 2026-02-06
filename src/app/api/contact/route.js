92b17674cc49abec9ae5374e284e252bcc495949
Abisha <abishawahab@gmail.com>
2026-02-06 20:06:39 +0500

    Update route.js

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -23,6 +23,7 @@ export async function POST(req) {
         <h3>New Contact Message</h3>
         <p><b>Name:</b> ${name}</p>
         <p><b>Email:</b> ${email}</p>
+        <p><b>Subject:</b> ${subject}</p>
         <p><b>Message:</b><br/>${message}</p>
       `,
     });
